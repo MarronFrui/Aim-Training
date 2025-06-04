cd031a0c522af392cbecada03e8ea93b954a0a78
MarronFrui <maxime.fels@gmail.com>
2025-06-04 14:25:21 +0200

    Removing do/while in spawntarget() | Disable targets spawn location rules

--- gamemode/classicmode.js
+++ gamemode/classicmode.js
@@ -5,34 +5,29 @@ export function spawnTarget(container, targetsPositions, onTargetHit) {
   const targetSize = 40;
 
   const scoreZone = {
-    x: window.innerWidth - 150,
+    x: window.innerWidth - 200,
     y: 0,
-    width: 150,
-    height: 60,
+    width: 200,
+    height: 80,
   };
 
   let x, y;
   let tries = 0;
-  const maxTries = 100;
 
-  do {
-    x = Math.random() * (window.innerWidth - targetSize);
-    y = Math.random() * (window.innerHeight - targetSize);
+  x = Math.random() * (window.innerWidth - targetSize);
+  y = Math.random() * (window.innerHeight - targetSize);
 
-    const overlapsScoreZone =
-      x + targetSize > scoreZone.x && y < scoreZone.height;
+  // Need to think another way to deal with exceptions
 
-    const overlapsOtherTarget = targetsPositions.some((pos) => {
-      const dx = pos.x - x;
-      const dy = pos.y - y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-      return distance < targetSize;
-    });
+  // const overlapsScoreZone =
+  //   x + targetSize > scoreZone.x && y < scoreZone.height;
 
-    if (!overlapsScoreZone && !overlapsOtherTarget) break;
-
-    tries++;
-  } while (tries < maxTries);
+  // const overlapsOtherTarget = targetsPositions.some((pos) => {
+  //   const dx = pos.x - x;
+  //   const dy = pos.y - y;
+  //   const distance = Math.sqrt(dx * dx + dy * dy);
+  //   return distance < targetSize;
+  // });
 
   target.style.left = `${x}px`;
   target.style.top = `${y}px`;
